8ea2d44877cf1ed9b8374ba9d729c164dbe0e0d8
ahermanoml <mascarenhasluz@gmail.com>
2023-09-20 06:28:09 -0300

    -A

--- myscript.js
+++ myscript.js
@@ -18,7 +18,7 @@ let numb1;
 let numb2;
 let operator;
 
-function operate (numb1, numb2, operator) {
+function operate () {
     switch (operator) {
         case "+":
         return add(numb1, numb2);
@@ -33,6 +33,43 @@ function operate (numb1, numb2, operator) {
         return divide(numb1, numb2);
         break;
         default:
-        alert("Somethign went wrong");
+        alert("Something went wrong");
     }
 }
+
+let display = document.querySelector('#display')
+
+let butt1 = document.querySelector('#one');
+let one = butt1.innerText;
+
+
+let butt2 = document.querySelector('#two');
+let two = butt2.innerText;
+
+let buttM = document.querySelector('#multiply');
+
+
+
+butt1.addEventListener("click", function() {
+    numb1 = butt1.innerText;
+    console.log(numb1);
+    display.innerText += one;
+    return numb1;
+});
+
+butt2.addEventListener("click", function() {
+    numb2 = butt2.innerText;
+    console.log(numb2);
+    display.innerText += two;
+    return numb2;
+});
+
+buttM.addEventListener("click", function() {
+    operator = buttM.innerText;
+    console.log(operator);
+    return operator;
+});
+
+
+
+
